47ea45371291834d21ac48971b1001b8af99764d
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2020-12-03 21:57:00 +0400

    made everything 10x more formal

--- maths-helper.py
+++ maths-helper.py
@@ -1,10 +1,10 @@
 from math import sqrt
-action = input("what do you wanna do bro? ")
+action = input("What do you want me to do? ")
 
 if action == "calculator":
     operation = input("operation? ")
-    num1 = int(input("whats the first number dude? "))
-    num2 = int(input("whats the second number bro? "))
+    num1 = float(input("First Number? "))
+    num2 = float(input("Second Number? "))
     answer = 0
     if operation == "+":
         answer = num1 + num2
@@ -22,33 +22,33 @@ if action == "calculator":
         answer = num1 % num2
         print('Answer is: ' + str(answer))
     else:
-        print("are you sure about that or are you just really dumb")
+        print("Not sure what to do there. Check if you have any typos.")
 elif action == "perimeter":
     peri_answer = 0
-    rec_or_squ = input("square or rectangle broooo? ")
+    rec_or_squ = input("Square or Rectangle? ")
     if rec_or_squ == "rectangle":
-        rec_leng = int(input("whats is the length of the rectangle dudebro? "))
-        rec_brea = int(input("whats the breadth of the rectangle dudebroboy? "))
+        rec_leng = float(input("What is the length of the rectangle? "))
+        rec_brea = float(input("What is the breadth of the rectangle? "))
         peri_answer = 2*(rec_leng+rec_brea)
     elif rec_or_squ == "square":
-        squ_side = int(input("whats the side of the square huh? "))
+        squ_side = float(input("What is the side of the square? "))
         peri_answer = squ_side*4
         print("The Answer is: " + str(peri_answer)+(" units"))
 elif action == "area":
     area_answer = 0
-    area__rec_or_squ = input("square or rectangle dude? ")
+    area__rec_or_squ = input("Square or Rectangle? ")
     if area__rec_or_squ == "rectangle":
-        area__rec_leng = int(input("what is the length of rectangle dude? "))
-        area__rec_brea = int(input("what is the breadth of the rectangle dudebroguy? "))
+        area__rec_leng = float(input("What is the length of the rectangle? "))
+        area__rec_brea = float(input("What is the breadth of the rectangle? "))
         area_answer = area__rec_leng*area__rec_brea
         print("The answer is: " + str(area_answer) + (" units"))
     elif area__rec_or_squ == "square":
-        area__squ_side = int(input("what is the side of the square broooooo? "))
+        area__squ_side = float(input("What is the side of the square? "))
         area_answer = area__squ_side*area__squ_side
         print("The Answer is: " + str(area_answer) + (" units"))
 elif action == "square root":
-    square_root = input("hey whats the square root? ")
-    squ_root = sqrt(int(square_root))
-    print((squ_root))
+    square_root = input("What is the number you want to find the square root of? ")
+    squ_root = sqrt(float(square_root))
+    print(squ_root)
 else:
-    print("check your spelling cuz idk wtf that means")
+    print("Not sure what to do there. Check if you have any typos.")
