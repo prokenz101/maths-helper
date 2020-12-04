0372d9b371f8964e742f226b050c31584e7397a0
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2020-12-04 19:42:30 +0400

    Added quit function to stop using script

--- maths-helper.py
+++ maths-helper.py
@@ -2,53 +2,78 @@ from math import sqrt
 action = input("What do you want me to do? ")
 
 if action == "calculator":
-    operation = input("operation? ")
-    num1 = float(input("First Number? "))
-    num2 = float(input("Second Number? "))
+    operation = input("Operation? ")
+    if operation == "quit":
+        exit()
+    num1 = input("What is the first number? ")
+    if num1 == "quit":
+        exit()
+    num2 = input("What is the second number? ")
+    if num2 == "quit":
+        exit()
     answer = 0
     if operation == "+":
-        answer = num1 + num2
+        answer = float(num1) + float(num2)
         print('Answer is: ' + str(answer))
     elif operation == "-":
-        answer = num1 - num2
+        answer = float(num1) - float(num2)
         print('Answer is: ' + str(answer))
     elif operation == "*":
-        answer = num1 * num2
+        answer = float(num1) * float(num2)
         print('Answer is: ' + str(answer))
     elif operation == "/":
-        answer = num1 / num2
+        answer = float(num1) / float(num2)
         print('Answer is: ' + str(answer))
     elif operation == "%":
-        answer = num1 % num2
+        answer = float(num1) % float(num2)
         print('Answer is: ' + str(answer))
-    else:
-        print("Not sure what to do there. Check if you have any typos.")
 elif action == "perimeter":
     peri_answer = 0
     rec_or_squ = input("Square or Rectangle? ")
+    if rec_or_squ == "quit":
+        exit()
     if rec_or_squ == "rectangle":
-        rec_leng = float(input("What is the length of the rectangle? "))
-        rec_brea = float(input("What is the breadth of the rectangle? "))
-        peri_answer = 2*(rec_leng+rec_brea)
+        rec_leng = input("What is the length of the rectangle? ")
+        if rec_leng == "quit":
+            exit()
+        rec_brea = input("What is the breadth of the rectangle? ")
+        if rec_brea == "quit":
+            exit()
+        peri_answer = 2 * (float(rec_leng) + float(rec_brea))
+        print(f'The Answer is: {peri_answer} units')
     elif rec_or_squ == "square":
-        squ_side = float(input("What is the side of the square? "))
-        peri_answer = squ_side*4
+        squ_side = input("What is the side of the square? ")
+        if squ_side == "quit":
+            exit()
+        peri_answer = float(squ_side) * 4
         print("The Answer is: " + str(peri_answer)+(" units"))
 elif action == "area":
     area_answer = 0
     area__rec_or_squ = input("Square or Rectangle? ")
-    if area__rec_or_squ == "rectangle":
-        area__rec_leng = float(input("What is the length of the rectangle? "))
-        area__rec_brea = float(input("What is the breadth of the rectangle? "))
-        area_answer = area__rec_leng*area__rec_brea
-        print("The answer is: " + str(area_answer) + (" units"))
-    elif area__rec_or_squ == "square":
-        area__squ_side = float(input("What is the side of the square? "))
-        area_answer = area__squ_side*area__squ_side
-        print("The Answer is: " + str(area_answer) + (" units"))
+    if area__rec_or_squ == "quit":
+        exit()
+    else:
+        if area__rec_or_squ == "rectangle":
+            if area__rec_or_squ == "quit":
+                exit()
+            area__rec_leng = input("What is the length of the rectangle? ")
+            if area__rec_leng == "quit":
+                exit()
+            area__rec_brea = input("What is the breadth of the rectangle? ")
+            if area__rec_brea == "quit":
+                exit()
+            area_answer = float(area__rec_leng) * float(area__rec_brea)
+            print("The Answer is: " + str(area_answer) + (" square units"))
+        elif area__rec_or_squ == "square":
+            area__squ_side = input("What is the side of the square? ")
+            if area__squ_side == "quit":
+                exit()
+            area_answer = float(area__squ_side) * float(area__squ_side)
+            print("The Answer is: " + str(area_answer) + (" square units"))
 elif action == "square root":
     square_root = input("What is the number you want to find the square root of? ")
-    squ_root = sqrt(float(square_root))
-    print(squ_root)
-else:
-    print("Not sure what to do there. Check if you have any typos.")
+    if square_root == "quit":
+        exit()
+    else:
+        squ_root = sqrt(float(square_root))
+        print(f'The Answer is: {squ_root}')
